7a6468c5cd884b91c7a04b51b43c97c0041a083f
Ocks <milomgenova@gmail.com>
2024-12-03 15:16:19 -0500

    still so fucking slowwwww

--- Assets/OcksTools/Scripts/Misc/CorruptionCode.cs
+++ Assets/OcksTools/Scripts/Misc/CorruptionCode.cs
@@ -8,7 +8,6 @@ using static UnityEditor.PlayerSettings;
 public class CorruptionCode : MonoBehaviour
 {
     Dictionary<Vector2Int, VoidTile> allnerds = new Dictionary<Vector2Int, VoidTile>();
-    Dictionary<Vector2Int, VoidTile> activenerds = new Dictionary<Vector2Int, VoidTile>();
     public GameObject VoidObject;
     Thread head;
     public static CorruptionCode Instance;
@@ -17,164 +16,102 @@ public class CorruptionCode : MonoBehaviour
         Instance = this;
         head = Thread.CurrentThread;
     }
-
+    
     public IEnumerator ClearAllNerds()
     {
-        foreach(var a in allnerds)
-        {
-            if (a.Value.VoidObject != null)
-            a.Value.VoidObject.transform.position = new Vector3 (0, 1000000000, 10000);
-        }
-        var we = new Dictionary<Vector2Int, VoidTile>(allnerds);
+        var dupe = new Dictionary<Vector2Int, VoidTile>(allnerds);
         allnerds.Clear();
-        int amtper = 1;
-        int i = 0;
-        foreach(var a in we)
+        Tags.refs[GetCurretHolder(holder)].SetActive(false);
+        holder += 1;
+        var cd = Tags.refs[GetCurretHolder(holder %= 2)].transform;
+        cd.gameObject.SetActive(true);
+
+        foreach (var a in dupe)
         {
-            i++;
-            KillObj(a.Key, a.Value.VoidObject);
-            if (i >= amtper)
+            if (a.Value.iscomplete && !a.Value.isend)
             {
-                i = 0;
                 yield return new WaitForFixedUpdate();
+                a.Value.VoidObject.transform.position = new Vector3(0, 987540987, 4795433);
+                a.Value.VoidObject.transform.parent = cd;
+                poolpps.Add(a.Value.VoidObject, 1);
             }
         }
 
+        yield break;
     }
-    public void KillObj(Vector2Int pos, GameObject VoidObject)
+    int holder = 0;
+    public string GetCurretHolder(int holder)
     {
-        if(VoidObject != null)
-            VoidObject.SetActive(true);
-        if (allnerds.ContainsKey(pos)) allnerds.Remove(pos);
+        return $"VoidHolder{holder}";
     }
 
     public void CorruptTile(Vector2Int pos)
     {
         StartCoroutine(cortile(pos));
     }
-    float maxtim = 2f;
+
     float mintim = 0.35f;
+    float maxtim = 2f;
+    private static List<Vector2Int> nerdlocs = new List<Vector2Int>()
+    {
+        new Vector2Int(1,1),
+        new Vector2Int(1,-1),
+        new Vector2Int(-1,1),
+        new Vector2Int(-1,-1),
+        new Vector2Int(0,1),
+        new Vector2Int(0,-1),
+        new Vector2Int(1,0),
+        new Vector2Int(-1,0),
+    };
     private IEnumerator cortile(Vector2Int pos)
     {
         if (allnerds.ContainsKey(pos)) goto wa;
         if(!Gamer.Instance.IsPosInBounds(twotothree(pos), true, true)) goto wa;
         var ween = new VoidTile();
-        ween.timer = Random.Range(mintim, maxtim)*0.9f;
         ween.pos = pos;
-        ween.iscomplete = false;
+        ween.timer = Random.Range(mintim, maxtim);
         allnerds.Add(pos, ween);
-        activenerds.Add(pos, ween);
-        yield return new WaitForSeconds(ween.timer);
-        if (!allnerds.ContainsKey(pos))
-        {
-            goto wa;
-        }
-        CompleteCorruption(pos);
-        goto skibidi;
-        yield return new WaitForSeconds(maxtim);
-        if (!allnerds.ContainsKey(pos))
-        {
-            KillObj(pos, ween.VoidObject);
-            goto wa;
-        }
-        float minfade = 0.7f;
-        float maxfade = 3f;
-        var aaa = ween.VoidObject.GetComponent<SpriteRenderer>();
-        var c = aaa.color;
-        c *= 0.92f;
-        c.a = 1;
-        aaa.color = c;
-        yield return new WaitForSeconds(Random.Range(minfade, maxfade));
-        if (!allnerds.ContainsKey(pos))
-        {
-            KillObj(pos, ween.VoidObject);
-            goto wa;
-        }
-        c = aaa.color;
-        c *= 0.92f;
-        c.a = 1;
-        aaa.color = c;
-        yield return new WaitForSeconds(Random.Range(minfade, maxfade));
-        if (!allnerds.ContainsKey(pos))
-        {
-            KillObj(pos, ween.VoidObject);
-            goto wa;
-        }
-        c = aaa.color;
-        c *= 0.90f;
-        c.a = 1;
-        aaa.color = c;
-        yield return new WaitForSeconds(Random.Range(minfade, maxfade));
-        if (!allnerds.ContainsKey(pos))
-        {
-            KillObj(pos, ween.VoidObject);
-            goto wa;
-        }
-        c = aaa.color;
-        c *= 0.87f;
-        c.a = 1;
-        aaa.color = c;
-        yield return new WaitForSeconds(Random.Range(minfade, maxfade));
-        if (!allnerds.ContainsKey(pos))
-        {
-            KillObj(pos, ween.VoidObject);
-            goto wa;
-        }
-        c = aaa.color;
-        c *= 0.84f;
-        c.a = 1;
-        aaa.color = c;
-        skibidi:
-        ween.iscomplete = true;
-        yield return new WaitForSeconds(8);
-        if (!allnerds.ContainsKey(pos))
-        {
-            KillObj(pos, ween.VoidObject);
-            goto wa;
-        }
-        List<Vector2Int> possy = new List<Vector2Int>() 
-        {
-            pos + new Vector2Int(1, 1),
-            pos + new Vector2Int(-1, 1),
-            pos + new Vector2Int(1, -1),
-            pos + new Vector2Int(-1, -1),
-            pos + new Vector2Int(1, 0),
-            pos + new Vector2Int(-1, 0),
-            pos + new Vector2Int(0, 1),
-            pos + new Vector2Int(0, -1),
-        };
-        if (TileExists(possy[0]) && TileExists(possy[1]) && TileExists(possy[2]) && TileExists(possy[3]) && TileExists(possy[4]) && TileExists(possy[5]) && TileExists(possy[6]) && TileExists(possy[7]))
-        {
-            foreach(var p in possy)
-            {
-                KillObj(p, allnerds[p].VoidObject);
-                allnerds.Remove(p);
-            }
-            ween.VoidObject.transform.localScale = new Vector3(3,3,1);
 
+        yield return new WaitForSeconds(ween.timer);
+        ween.VoidObject = PullFromPool(pos);
+        ween.iscomplete = true;
+        var nerdlocs2 = new List<Vector2Int>(nerdlocs);
+        for(int i = 0; i < nerdlocs2.Count; i++)
+        {
+            nerdlocs2[i] += pos;
+        }
+        CorruptTile(nerdlocs2[4]);
+        CorruptTile(nerdlocs2[5]);
+        CorruptTile(nerdlocs2[6]);
+        CorruptTile(nerdlocs2[7]);
+
+        yield return new WaitForSeconds(8);
+        if (ween.isend) goto wa; 
+        bool e = true;
+        foreach(var popo in nerdlocs2)
+        {
+            if (!TileExists(popo))
+            {
+                e = false; break;
+            }
+        }
+        if (e)
+        {
+            foreach (var popo in nerdlocs2)
+            {
+                allnerds[popo].VoidObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                ShadowRealm(allnerds[popo]);
+            }
+            ween.VoidObject.GetComponent<SpriteRenderer>().color = Color.green;
+            ween.VoidObject.transform.localScale = Vector3.one * 3; 
         }
 
 
     wa:;
     }
-
     public bool TileExists(Vector2Int pos)
     {
-        return allnerds.ContainsKey(pos);
-    }
-    Dictionary<GameObject, int> sussypool = new Dictionary<GameObject, int>();
-
-    public GameObject PullFromPool(Vector2Int pos)
-    {
-        if(sussypool.Count > 0)
-        {
-            var a = sussypool.ElementAt(0);
-            a.Key.transform.localScale = Vector3.one;
-            sussypool.Remove(a.Key);
-            a.Key.SetActive(true);
-            return a.Key;
-        }
-        return Instantiate(VoidObject, twotothree(pos), Quaternion.identity, Tags.refs["VoidHolder"].transform);
+        return allnerds.ContainsKey(pos) && allnerds[pos].iscomplete && !allnerds[pos].isend;
     }
 
     public static Vector3 twotothree(Vector2Int pos)
@@ -184,7 +121,6 @@ public class CorruptionCode : MonoBehaviour
 
     public void CompleteCorruption(Vector2Int pos)
     {
-        activenerds.Remove(pos);
         var ween = allnerds[pos];
         ween.VoidObject = PullFromPool(pos);
         CorruptTile(pos+new Vector2Int(0,1));
@@ -192,6 +128,35 @@ public class CorruptionCode : MonoBehaviour
         CorruptTile(pos+new Vector2Int(1,0));
         CorruptTile(pos+new Vector2Int(-1,0));
     }
+
+    Dictionary<GameObject, int> poolpps = new Dictionary<GameObject, int>();
+
+    public GameObject PullFromPool(Vector2Int pos)
+    {
+        if(poolpps.Count > 0)
+        {
+            var pp = poolpps.ElementAt(0);
+            poolpps.Remove(pp.Key);
+            pp.Key.transform.position = twotothree(pos);
+            pp.Key.transform.localScale = Vector3.one;
+            return pp.Key;
+        }
+        return Instantiate(VoidObject, twotothree(pos), Quaternion.identity, Tags.refs[GetCurretHolder(holder)].transform);
+    }
+    public void ShadowAndErase(VoidTile vt)
+    {
+        allnerds.Remove(vt.pos);
+        ShadowRealm(vt);
+    }
+
+    public void ShadowRealm(VoidTile vt)
+    {
+        vt.VoidObject.transform.position = new Vector3(0,987540987,4795433);
+        vt.iscomplete = true;
+        vt.isend = true;
+        poolpps.Add(vt.VoidObject, 0);
+    }
+
 
 }
 
@@ -201,5 +166,6 @@ public class VoidTile
     public Vector2Int pos;
     public float timer = 0;
     public bool iscomplete = false;
+    public bool isend = false;
     public GameObject VoidObject;
 }
